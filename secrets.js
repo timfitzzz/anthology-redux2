c8c3e46287c34779a4e56fe19dc08ce23a139752
Dicey <diceytroop@gmail.com>
2016-01-05 20:13:01 -0500

    Delete secrets.js

--- secrets.js
+++
@@ -1,3 +0,0 @@
-export const TWITTER_KEY = "FAewydtVjtwsW9aClF4sPeIpi";
-export const TWITTER_SECRET = "4WM85V6ClDUrOM9gyPqauJrVETW3OUsfzlFORAv3moixrpan6I";
-export const TWITTER_CALLBACK = "http://127.0.0.1:3002/auth/twitter/callback";
